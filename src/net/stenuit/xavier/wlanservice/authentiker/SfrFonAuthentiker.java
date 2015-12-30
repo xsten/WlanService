2f7a59b7030690d58e5a5e0150d2f1877fb5bb54
Xavier Stenuit <xsgit@herm25.com>
2015-12-30 16:15:33 +0100

    New authenticater

---
+++ src/net/stenuit/xavier/wlanservice/authentiker/SfrFonAuthentiker.java
@@ -0,0 +1,10 @@
+package net.stenuit.xavier.wlanservice.authentiker;
+
+public class SfrFonAuthentiker extends BelgacomPostHttpTask {
+
+	@Override
+	public String getPropertiesKey()
+	{
+		return "SFR WiFi FON";
+	}
+}
